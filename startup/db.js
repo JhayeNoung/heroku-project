c7fc4365b8588b7509f5c93ae2f1f2ad7bda32f5
James <james.noung@gmail.com>
2024-09-09 06:03:59 +0700

    db

--- startup/db.js
+++ startup/db.js
@@ -1,17 +1,10 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const {msgLogger} = require('../middlewares/logger');
-const fixieData = process.env.FIXIE_SOCKS_HOST.split(new RegExp('[/(:\\/@/]+'));
 
 const db = config.get('db');
 module.exports = function(){
-    mongoose.connect(db, {
-            proxyUsername: fixieData[0],
-            proxyPassword: fixieData[1],
-            proxyHost: fixieData[2],
-            proxyPort: fixieData[3]
-        }
-    )
+    mongoose.connect(db)
     .then(()=>msgLogger.info(`Connect to ${db}`))
     .catch(err => msgLogger.error(err.message));   
 }
